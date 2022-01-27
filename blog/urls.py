08e1d6b9098a55fe357b08f897a336402e6b13c7
Asadbek <84005743+SolijonovAsadbek@users.noreply.github.com>
2022-01-28 00:01:39 +0500

    post news

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from blog.views import BlogListView, PostDetail
+from blog.views import BlogListView, PostDetail, search
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
-    path('post/<int:pk>/', PostDetail.as_view(), name='postdetail')
+    path('post/<int:pk>/', PostDetail.as_view(), name='postdetail'),
+    path('search/', search, name='search')
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,17 @@ from django.views.generic import ListView, DetailView
 # Create your views here.
 from blog.models import Post
 
+object_list = ""
+
+
+def search(request):
+    global object_list
+    req = request.GET.get('title', '')
+    if req and req != "":
+        object_list = Post.objects.filter(title__contains=req).all()
+
+    return render(request, 'base.html', {'title': req, 'object_list': object_list})
+
 
 class BlogListView(ListView):
     model = Post
