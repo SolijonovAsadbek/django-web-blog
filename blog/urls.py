8f4bc39e37734190c969042e28f2a667b299698c
Asadbek <84005743+SolijonovAsadbek@users.noreply.github.com>
2022-02-11 23:14:53 +0500

    web post

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from blog.views import BlogListView, PostDetail, search
+from blog.views import BlogListView, PostDetail, search, PostCreate
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
     path('post/<int:pk>/', PostDetail.as_view(), name='postdetail'),
-    path('search/', search, name='search')
+    path('search/', search, name='search'),
+    path('post/new', PostCreate.as_view(), name='newpost'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 # Create your views here.
 from blog.models import Post
@@ -8,19 +8,31 @@ object_list = ""
 
 
 def search(request):
-    global object_list
+    global object_list, articles
     req = request.GET.get('title', '')
+    print(req)
     if req and req != "":
         object_list = Post.objects.filter(title__contains=req).all()
+        articles = len(object_list)
+    else:
+        articles = 0
 
-    return render(request, 'base.html', {'title': req, 'object_list': object_list})
+    return render(request, 'base.html',
+                  {'title': req, 'object_list': object_list, "articles": articles})
 
 
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+    articles = Post.objects.count()
 
 
 class PostDetail(DetailView):
     model = Post
     template_name = 'postdetail.html'
+
+
+class PostCreate(CreateView):
+    model = Post
+    template_name = 'newpost.html'
+    fields = ['title', 'body', 'author']
